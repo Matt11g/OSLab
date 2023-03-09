7f6a59e970ad60d12347f733a8fd4de3da3f1aef
tracer-nju <tracer@nju.edu.cn>
2023-03-09 18:55:30 +0800

    Linux matt-pc 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:55:30 up 52 min,  3 users,  load average: 0.42, 0.22, 0.22

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -8,11 +8,11 @@
 #include <stdlib.h>
 
 void display_version() {
-  printf("pstree (PSmisc) 23.4\n
+  printf("pstree (PSmisc) 23.4\n\
 					Copyright (C) 1993-2020 Werner Almesberger and Craig Small\n
-					\n				
-					PSmisc comes with ABSOLUTELY NO WARRANTY.\n
-					This is free software, and you are welcome to redistribute it under	the terms of the GNU General Public License.\n
+					\n\
+					PSmisc comes with ABSOLUTELY NO WARRANTY.\n\
+					This is free software, and you are welcome to redistribute it under	the terms of the GNU General Public License.\n\
 					For more information about these matters, see the files named COPYING.\n");
 }
 
