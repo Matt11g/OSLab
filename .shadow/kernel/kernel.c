192921b9fb3767508fe8caed9b65036eba8dace5
tracer-nju <tracer@nju.edu.cn>
2023-03-18 20:40:20 +0800

    Linux matt-pc 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:40:20 up 21:34,  4 users,  load average: 0.12, 0.19, 0.19

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -30062,12 +30062,12 @@ void splash() {
   ioe_read(AM_GPU_CONFIG, &info);
   w = info.width;
   h = info.height;  
-  for (int x = 0; x * SIDE <= w; x ++) {
-    for (int y = 0; y * SIDE <= h; y++) {
+  for (int x = 0; x * SIDE <= h; x ++) {
+    for (int y = 0; y * SIDE <= w; y++) {
       //if ((x & 1) ^ (y & 1)) {
         //draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
       //}
-			draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, get_rgb(x * ww / w * hh + hh - y * hh / h));
+			draw_tile(y * SIDE, x * SIDE, SIDE, SIDE, get_rgb(x * ww / w * ww + y * hh / h));
     }
   }
   /*for (int x = 0; x * SIDE < MIN(ww, w); x ++) {
